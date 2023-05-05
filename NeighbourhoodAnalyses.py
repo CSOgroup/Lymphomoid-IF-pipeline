bf76b879911be98917aef394d399409d42581492
danieletavernari <tavernaridaniele@gmail.com>
2023-05-05 18:00:36 +0200

    Implemented obs vs exp

--- NeighbourhoodAnalyses.py
+++ NeighbourhoodAnalyses.py
@@ -43,12 +43,15 @@ if consider_othercells:
 else:
     nei_df_all = pd.DataFrame(columns=[ "Bcells","CD4","CD8","Macrophages","ll" ])
 dist_df_all = nei_df_all.copy()
+df_all = pd.DataFrame(columns=[ "CellType_marker","ll" ])
 
 for ll in lymphomoids_to_process:
     print("\n")
     print("------ Processing ", ll," ------")
     # Reading and preprocessing table
     df = pd.read_table( args.main_dir+'Classified_cells_tables/Table_'+ll+'_AllCells.txt' )
+    # df = pd.read_table( '/mnt/ndata/daniele/elisa_lymphomoids/Processed/Pipeline_test/Classified_cells_tables/Table_'+ll+'_AllCells.txt' )
+    
     if not consider_othercells:
         df = df.loc[df.CellType_marker != "otherCell",:]
     df.CellType_marker = df.CellType_marker.str.replace(" ","")
@@ -77,8 +80,10 @@ for ll in lymphomoids_to_process:
     adata.obs.loc[cells_boundary,'is_boundary'] = "yes"
     if plot_delaunay:
         sc.pl.spatial(adata, neighbors_key="spatial_neighbors", spot_size=5, color='CellType_marker', palette=this_palette, edges=True, edges_width=0.05, edges_color='#EBEBEB', show=False)
+        plt.tight_layout()
         plt.savefig(args.main_dir+'/Neighbourhood_analyses/'+args.custom_file_prefix+ll+'_delaunay_plot_afterFiltering.pdf')
         sc.pl.spatial(adata, neighbors_key="spatial_neighbors", spot_size=5, color='is_boundary', palette={"yes":"red","no":"#EBEBEB"}, edges=True, edges_width=0.05, edges_color='#EBEBEB', show=False)
+        plt.tight_layout()
         plt.savefig(args.main_dir+'/Neighbourhood_analyses/'+args.custom_file_prefix+ll+'_delaunay_plot_Boundary.pdf')
     if args.celltype_query_proliferation_status=="proliferating":
         query_cells = adata.obs.loc[(adata.obs['CellType_marker']==args.celltype_query) & (adata.obs['is_proliferating']),].index
@@ -90,10 +95,21 @@ for ll in lymphomoids_to_process:
         query_cells = adata.obs.loc[(adata.obs['CellType_marker']==args.celltype_query),].index
         title_neighComp = ll+",\nneighbourhood of "+args.celltype_query
     query_cells = pd.Index(list(set(query_cells)-set(cells_boundary)))
+    if (len(query_cells)<5):
+        print("Skipping", ll,", less than 5 query",args.celltype_query,"after subsetting for proliferation status")
+        continue
     if consider_othercells:
         nei_df = pd.DataFrame(np.NaN,query_cells,[ "Bcells","CD4","CD8","Macrophages","otherCell" ])
+        this_df = df.loc[:,["CellType_marker","is_proliferating"] ]
+        this_df['ll'] = ll
+        this_df = this_df.drop(columns="is_proliferating")
+        df_all = pd.concat([df_all,this_df],axis=0)
     else:
         nei_df = pd.DataFrame(np.NaN,query_cells,[ "Bcells","CD4","CD8","Macrophages" ])
+        this_df = df.loc[df.CellType_marker!="otherCell",["CellType_marker","is_proliferating"] ]
+        this_df['ll'] = ll
+        this_df = this_df.drop(columns="is_proliferating")
+        df_all = pd.concat([df_all,this_df],axis=0)
     dist_df = nei_df.copy()
 
     query_spatial_conn = adata.obsp['spatial_connectivities'][:,:].toarray()
@@ -116,6 +132,7 @@ for ll in lymphomoids_to_process:
     sns.barplot(data=nei_df_melt, x="Neighbouring cell type", y="Relative frequency", palette=this_palette)
     plt.ylim(0, 1)
     plt.title( title_neighComp )
+    plt.tight_layout()
     plt.savefig(args.main_dir+'/Neighbourhood_analyses/'+args.custom_file_prefix+ll+'_NeighbourhoodFrequencies.pdf')
     plt.close()
     nei_df.to_csv(args.main_dir+'/Neighbourhood_analyses/'+args.custom_file_prefix+ll+'_NeighbourhoodFrequencies_FullTable.csv')
@@ -126,6 +143,7 @@ for ll in lymphomoids_to_process:
     dist_df_melt.columns = [ "Neighbouring cell type","Distance (µm)" ]
     plt.figure()
     sns.kdeplot(data=dist_df_melt, hue="Neighbouring cell type", x="Distance (µm)",linewidth=2, palette=this_palette)
+    plt.tight_layout()
     plt.savefig(args.main_dir+'/Neighbourhood_analyses/'+args.custom_file_prefix+ll+'_DistanceDistributions.pdf')
     plt.close()
     dist_df.to_csv(args.main_dir+'/Neighbourhood_analyses/'+args.custom_file_prefix+ll+'_DistanceDistributions_FullTable.csv')
@@ -139,13 +157,46 @@ for ll in lymphomoids_to_process:
 ## Second, collapse lymphomoids
 nei_df_all['PatientLymphomoidName'] = [thisll[::-1].partition('_')[0][::-1] for thisll in nei_df_all['ll']]
 dist_df_all['PatientLymphomoidName'] = [thisll[::-1].partition('_')[0][::-1] for thisll in dist_df_all['ll']]
+df_all['PatientLymphomoidName'] = [thisll[::-1].partition('_')[0][::-1] for thisll in df_all['ll']]
 nei_df_all.to_csv(args.main_dir+'/Neighbourhood_analyses/'+args.custom_file_prefix+'AllLymphomoids_NeighbourhoodFrequencies_FullTable.csv')
 dist_df_all.to_csv(args.main_dir+'/Neighbourhood_analyses/'+args.custom_file_prefix+'AllLymphomoids_DistanceDistributions_FullTable.csv')
 
+# nei_df_all = pd.read_csv( "/mnt/ndata/daniele/elisa_lymphomoids/Processed/Pipeline_test/Neighbourhood_analyses/LP08Any_AllLymphomoids_NeighbourhoodFrequencies_FullTable.csv" )
+nei_df_all.loc[nei_df_all.Bcells.isna(),"Bcells"] = 0
+nei_df_all.loc[nei_df_all.CD4.isna(),"CD4"] = 0
+nei_df_all.loc[nei_df_all.CD8.isna(),"CD8"] = 0
+nei_df_all.loc[nei_df_all.Macrophages.isna(),"Macrophages"] = 0
+# nei_df_all = nei_df_all.loc[nei_df_all.ll.isin(df_all.ll.unique()),]
+
 # Plotting neigh composition
 nei_df_all = nei_df_all.drop(columns="ll")
+df_all = df_all.drop( columns = 'll' )
+# nei_df_all = nei_df_all.drop(columns="Unnamed: 0")
 dist_df_all = dist_df_all.drop(columns="ll")
+
 nei_df_grouped = nei_df_all.groupby(['PatientLymphomoidName']).mean()
+# nei_df_grouped.sum(1)
+
+# Obs vs Exp
+df_all_grouped = nei_df_grouped.copy()
+for i in df_all_grouped.index:
+    for j in df_all_grouped.columns:
+        df_all_grouped.loc[i,j] = np.sum((df_all.CellType_marker==j) & (df_all.PatientLymphomoidName==i) )
+df_all_grouped = df_all_grouped.div(df_all_grouped.sum(1),axis=0)
+obs_vs_exp_log_ratio = np.log2(nei_df_grouped/df_all_grouped)
+obs_vs_exp_log_ratio['PatientLymphomoidName'] = obs_vs_exp_log_ratio.index
+oe = pd.melt(obs_vs_exp_log_ratio,'PatientLymphomoidName')
+oe.columns = ["PatientLymphomoidName", "CellType","Obs_vs_Exp_log2_ratio" ]
+plt.style.use('classic')
+plt.figure()
+sns.barplot(data=oe, x="PatientLymphomoidName", y="Obs_vs_Exp_log2_ratio", hue="CellType", palette=this_palette)
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.savefig(args.main_dir+'/Neighbourhood_analyses/'+args.custom_file_prefix+'Obs_vs_Exp_log2_ratio.pdf')
+plt.close()
+oe.to_csv(args.main_dir+'/Neighbourhood_analyses/'+args.custom_file_prefix+'AllLymphomoids_Obs_vs_Exp_log2_ratio_Table.csv')
+
+# Barplot overall
 nei_df_grouped['PatientLymphomoidName'] = nei_df_grouped.index.values
 plt.style.use('classic')
 plt.figure()
@@ -163,7 +214,10 @@ if consider_othercells:
     values_otherCell = nei_df_grouped['otherCell'].values.tolist()
     ax.bar(groups, values_otherCell, bottom = np.add(np.add(np.add(values_Bcells,values_CD4),values_CD8),values_Macrophages), color = "gray", edgecolor = "black", linewidth = 1,label='otherCell')
 ax.legend(loc='lower right')
+plt.xticks(rotation=45)
 plt.ylabel('Relative frequency' )
+plt.tight_layout()
+# plt.savefig("/mnt/ndata/daniele/elisa_lymphomoids/Processed/Pipeline_test/Neighbourhood_analyses/LP08Any_AllLymphomoids_NeighbourhoodFrequencies_temp.pdf" )
 plt.savefig(args.main_dir+'/Neighbourhood_analyses/'+args.custom_file_prefix+'AllLymphomoids_NeighbourhoodFrequencies.pdf')
 plt.close()
 
@@ -171,6 +225,7 @@ for col in list(set(['Bcells','CD4','CD8','Macrophages','otherCell']) & set(dist
     this_dist_df_all = dist_df_all.loc[:,[col,"PatientLymphomoidName"]].copy().dropna()
     plt.figure()
     sns.kdeplot(data=this_dist_df_all, hue='PatientLymphomoidName', x=col,linewidth=2,palette="Set2")
+    plt.tight_layout()
     plt.savefig(args.main_dir+'/Neighbourhood_analyses/'+args.custom_file_prefix+'AllLymphomoids_'+col+'_DistanceDistributions.pdf')
     plt.close() 
 
